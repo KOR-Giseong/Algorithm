551011ebc0ecd7e218923c80ec9b5e9921bfed56
KOR-Giseong <75441722+KOR-Giseong@users.noreply.github.com>
2020-12-16 22:52:44 +0900

    달팽이 배열

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-x = int(input("줄 수를 입력하세요"))
-for i in range(1,x+1):
-        print("*"*i)
+n = int(input("숫자 입력 : "))
+
+for y in range(0, n):
+    for x in range(0, n):
+        p = min(x, y, n - x - 1, n - y - 1)
+        if x >= y:
+            q = x + y - 2 * p
+        else:
+            q = (n - 1 - 2 * p) * 4 - (x + y - 2 * p)
+        q += 4 * (p * n - (p * p))
+        print("{:3d}".format(q), end="")
+    print()
