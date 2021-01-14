e8172c45abf9997c55f24e91bfa41c9d0ac06e40
KOR-Giseong <75441722+KOR-Giseong@users.noreply.github.com>
2021-01-14 20:23:34 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def swap(x, i, j):
-    x[i], x[j] = x[j], x[i]
-
-def bubbleSort(x):
-    for size in reversed(range(len(x))):
-        for i in range(size):
-            if x[i] > x[i+1]:
-                swap(x, i, i+1)
